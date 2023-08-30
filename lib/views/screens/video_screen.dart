909850a95f9c40a41d7d546271fd053e52a8c77e
John Hamson <jhamson@techiemon.com>
2023-08-30 14:22:25 -0500

    cleanup

--- lib/views/screens/video_screen.dart
+++ lib/views/screens/video_screen.dart
@@ -229,26 +229,26 @@ class VideoScreen extends StatelessWidget {
                                       )
                                     ],
                                   ),
-                                  // Column(
-                                  //   children: [
-                                  //     InkWell(
-                                  //       onTap: () {},
-                                  //       child: const Icon(
-                                  //         Icons.reply,
-                                  //         size: 40,
-                                  //         color: Colors.white,
-                                  //       ),
-                                  //     ),
-                                  //     const SizedBox(height: 7),
-                                  //     Text(
-                                  //       data.shareCount.toString(),
-                                  //       style: const TextStyle(
-                                  //         fontSize: 20,
-                                  //         color: Colors.white,
-                                  //       ),
-                                  //     )
-                                  //   ],
-                                  // ),
+                                  Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.reply,
+                                          size: 40,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 7),
+                                      Text(
+                                        data.shareCount.toString(),
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                   CircleAnimation(
                                     child: buildMusicAlbum(publicProfileUrl),
                                   ),
